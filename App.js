6375fb68c2058fe094db8c6d0787d0f2f770a58a
akash-singh8 <developer.akash8@gmail.com>
2023-04-05 23:49:10 +0530

    Added fonts

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        "NotoSans-Bold": require("./assets/fonts/NotoSans-Bold.ttf"),
+        "NotoSans-Medium": require("./assets/fonts/NotoSans-Medium.ttf"),
+        "NotoSans-Regular": require("./assets/fonts/NotoSans-Regular.ttf"),
+        "Ubuntu-Medium": require("./assets/fonts/Ubuntu-Medium.ttf"),
+      });
+      setFontLoaded(true);
+    }
+
+    loadFonts();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    fontLoaded && (
+      <View style={styles.container}>
+        <Text style={{ fontFamily: "NotoSans-Medium" }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    )
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
